3925fc15cc2504ac7d4d3239bb186a9edc832459
Henry Okonkwo <henry@artnight.com>
2024-05-15 19:53:39 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div class="wrapper">
+  <h3>Under Construction</h3>
+  <img alt="Under Construction" src="./assets/under-construction.gif" class="logo" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+    height: 100vh;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: green;
+}
+h3 {
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+.logo {
+  display: block;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+}
+</style>
