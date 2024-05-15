1750803bdb8ec7e2e549d3eab05a2a30033f3242
Henry Okonkwo <henry@artnight.com>
2024-05-16 01:34:34 +0200

    update construction page

--- app.vue
+++ app.vue
@@ -4,29 +4,32 @@
 <template>
   <div class="wrapper">
   <h3>Under Construction</h3>
-  <img alt="Under Construction" src="./assets/under-construction.gif" class="logo" />
+  <img alt="Under Construction" src="./assets/b.gif" class="logo" />
   </div>
 </template>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
 .wrapper {
   display: flex;
-    height: 100vh;
+  height: 100vh;
     width: 100%;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background: green;
+    background: #EAEFF3;
 }
 h3 {
   text-align: center;
-  color: white;
+  color: #8393D1;
   font-size: 2rem;
-  margin-bottom: 2rem;
+  margin-bottom: 20px;
 }
 .logo {
   display: block;
-  width: 300px;
   height: 300px;
   border-radius: 50%;
 }
